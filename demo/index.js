c5d23102241ad363a34325da9d41edc8eb919c06
alexandr.buylov <buylovav@modulbank.ru>
2017-10-03 14:15:12 +0500

    add select

--- demo/index.js
+++ demo/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 import Page from './Page';
+import Select from './Select';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
     <Router history={browserHistory}>
         <Route path="/" component={Page} />
+        <Route path="/select" component={Select} />
     </Router>,
     document.getElementById('container')
 );
