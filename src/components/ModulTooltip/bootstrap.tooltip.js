2f8bd3990321a14b9a1431e89572ea5ae7f92c0e
Alexandr Buylov <alx2das@gmail.com>
2018-02-26 19:22:38 +0500

    fix

--- src/components/ModulTooltip/bootstrap.tooltip.js
+++ src/components/ModulTooltip/bootstrap.tooltip.js
@@ -347,7 +347,7 @@ if (typeof jQuery === 'undefined') {
 
     if (e.isDefaultPrevented()) return
 
-    $tip.animate({opacity: 0}, 200, function() {
+    $tip.animate({opacity: 0}, 150, function() {
 
         $tip.css({opacity: 100}).removeClass('in')
         $.support.transition && this.$tip.hasClass('fade') ?
